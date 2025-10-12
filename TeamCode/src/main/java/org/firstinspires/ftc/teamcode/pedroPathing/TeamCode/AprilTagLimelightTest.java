af14b531120b8cc52e1af89fcf6c52b0167df921
Jaidev Kaleeswaran <kaleeswaranjaidev@gmail.com>
2025-10-11 18:49:56 -0700

    Added code for AprilTagLimelightTest but has to be tested and finetuned on field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeamCode/AprilTagLimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeamCode/AprilTagLimelightTest.java
@@ -8,10 +8,13 @@ import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.pedroPathing.TeamCode.AprilTagLimelightTest;
 
 public class AprilTagLimelightTest extends OpMode {
     private Limelight3A limelight;
+    private double distance;
     private IMU imu;
+
     @Override
     public void init() {
         imu = hardwareMap.get(IMU.class, "imu");
@@ -33,9 +36,17 @@ public class AprilTagLimelightTest extends OpMode {
         LLResult llresult = limelight.getLatestResult();
         if (llresult != null && llresult.isValid()){
             Pose3D botpose = llresult.getBotpose_MT2();
+            distance = getDistanceFromTags(llresult.getTa());
+            telemetry.addData("ditance", distance);
             telemetry.addData("Tx", llresult.getTx());
             telemetry.addData("Tx", llresult.getTy());
             telemetry.addData("TA", llresult.getTa());
         }
+    }
+
+    public double getDistanceFromTags(double ta) {
+        double scale = 30000; //placeholder value
+        double distance = scale/ta;
+        return distance;
     }
 }
